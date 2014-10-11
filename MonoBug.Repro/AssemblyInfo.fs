c9dd3742f875042b69390e25f1544c4548f1a189
eiriktsarpalis <eirik.tsarpalis@gmail.com>
2014-10-11 18:04:59 +0300

    Initial check-in of module MonoBug.Repro

---
+++ MonoBug.Repro/AssemblyInfo.fs
@@ -0,0 +1,18 @@
+﻿module MonoBug.Repro.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<AssemblyTitle("MonoBug.Repro")>]
+[<AssemblyDescription("")>]
+[<AssemblyConfiguration("")>]
+[<AssemblyCompany("")>]
+[<AssemblyProduct("")>]
+[<AssemblyCopyright("eirik")>]
+[<AssemblyTrademark("")>]
+[<AssemblyVersion("1.0.0.0")>]
+()
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
---
+++ MonoBug.Repro/Program.fs
@@ -0,0 +1,56 @@
+﻿open System
+open System.Reflection
+open System.Reflection.Emit
+
+// create a new dynamic assembly
+
+let an = new AssemblyName("dynamicAssembly")
+let ab = AppDomain.CurrentDomain.DefineDynamicAssembly(an, AssemblyBuilderAccess.Run)
+let mb = ab.DefineDynamicModule("mainModule.dll")
+
+// define a generic class Foo<T> with public field T value
+
+let foob = mb.DefineType("Foo", TypeAttributes.Public)
+let t = foob.DefineGenericParameters([| "T" |]).[0]
+let fb = foob.DefineField("value", t, FieldAttributes.Public)
+
+let foo = foob.CreateType()
+let f = foo.GetField("value")
+
+// define a static class Bar with generic method Bar.Get : Foo<'T> -> 'T
+
+let barb = mb.DefineType("Bar", TypeAttributes.Public)
+
+let getB = barb.DefineMethod("Get", MethodAttributes.Public ||| MethodAttributes.Static)
+let tb = getB.DefineGenericParameters([| "T" |]).[0]
+getB.SetReturnType tb
+let fooT = foo.MakeGenericType [| tb :> Type |]
+getB.SetParameters([| fooT |])
+
+// emit method body
+
+let ilGen = getB.GetILGenerator()
+
+ilGen.Emit OpCodes.Ldarg_0
+ilGen.Emit(OpCodes.Ldfld, TypeBuilder.GetField(fooT, f))
+ilGen.Emit OpCodes.Ret
+
+let bar = barb.CreateType()
+
+// emission complete
+
+// parse opcodes using Mono.Reflection
+
+open Mono.Reflection
+
+let m = bar.GetMethod("Get")
+let instructions = m.GetInstructions()
+
+let field = 
+    instructions 
+    |> Seq.pick(fun i -> match i.Operand with :? FieldInfo as f -> Some f | _ -> None)
+
+
+printfn "%O" <| field.ToString() // "T item"
+
+printfn "%d" field.MetadataToken // crashes in mono  
